6e15519dbc5ceb5882a322229e742d62b068a068
vt3106nu <vuonga@my.normandale.edu>
2016-07-22 15:59:26 -0500

    Update HelloWorld.java
    this removes the integer variables

--- HelloWorld/src/greetings/HelloWorld.java
+++ HelloWorld/src/greetings/HelloWorld.java
@@ -5,12 +5,7 @@ public class HelloWorld
 
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
 		String message = "Hello world";
-=======
-		//put message variable
-		int number =3, otherNumber = 7;
->>>>>>> 7eb8fd1883adb93caa7b729e5833d3fb6aba8cd2
 	}
 
 }
